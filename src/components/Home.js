c9b685f87b78761ec8c9d7abf9942b5609451446
Gustavo Sanchez <Gustavo.Sanchez@thomsonreuters.com>
2020-06-25 20:34:37 -0300

    Escalabilidad en Home

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,19 +6,14 @@ import {DropdownButton, Dropdown} from 'react-bootstrap'
 import isEmpty from 'is-empty'
 import {Titulo} from './styledCommon'
 import * as Reducer from '../hookReducer/DataRecordsReducer'
+import {reportsTypes} from '../constants/reportsTypes'
+
 
 const Home=()=> {
     const [fetchState, localDispatch] = useReducer(Reducer.DataRecordsReducer, Reducer.initialState);
 
-    const leyendasTitulo=[
-        {"id":"registros", "description":"Reporte de Todos los registros"},
-        {"id":"reporteDiario", "description":"Reporte Diario"},
-        {"id":"reportePorCategoria", "description":"Reporte Gastos Por Categoria"},
-        {"id":"proyeccion", "description":"Proyección Mensual"},
-    ]
-
     const handleSelect=(e)=>{
-        localDispatch({ type: 'SET_CURRENT_DATA', data: fetchState.data[e], titulo:leyendasTitulo.find(x=>x.id==e).description });
+        localDispatch({ type: 'SET_CURRENT_DATA', data: fetchState.data[e], titulo:reportsTypes.find(x=>x.id==e).description });
     }
 
     useEffect(() => {
@@ -38,12 +33,8 @@ const Home=()=> {
         <div style={{margin:"5%"}}>
             <Titulo>{fetchState.titulo}</Titulo>
             <DropdownButton onSelect={handleSelect} id="dropdown-basic-button" title="Reportes" className="mb-3">
-                <Dropdown.Item eventKey="registros" as="button">
-                    Todos los Registros
-                </Dropdown.Item>
-                <Dropdown.Item eventKey="reporteDiario" as="button">
-                    Diario
-                </Dropdown.Item>
+                <Dropdown.Item eventKey="registros" as="button">Todos los Registros</Dropdown.Item>
+                <Dropdown.Item eventKey="reporteDiario" as="button">Diario</Dropdown.Item>
                 <Dropdown.Item eventKey="reportePorCategoria" as="button">Gastos por Categoria</Dropdown.Item>
                 <Dropdown.Item eventKey="proyeccion" as="button">Proyección Mensual</Dropdown.Item>
             </DropdownButton>
---
+++ src/constants/reportsTypes.js
@@ -0,0 +1,6 @@
+export const reportsTypes=[
+    {"id":"registros", "description":"Reporte de Todos los registros"},
+    {"id":"reporteDiario", "description":"Reporte Diario"},
+    {"id":"reportePorCategoria", "description":"Reporte Gastos Por Categoria"},
+    {"id":"proyeccion", "description":"Proyección Mensual"},
+]
